32978f4771c17dca43f1033926fbd6081a6fcb8a
meqdad <meqdad.moradi@outlook.com>
2022-02-22 13:44:37 +0100

    burger menu

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const toggler = document.querySelector(".toggler");
+const nav = document.querySelector(".nav");
 
 toggler.addEventListener("click", (e) => {
    e.currentTarget.classList.toggle("active");
+   if (toggler.classList.contains("active")) {
+      nav.classList.add("active");
+   } else {
+      nav.classList.remove("active");
+   }
 });
